d507cea961ae70f976ef86584034afe0fd0329fb
alexpapinutti <alexpapinutti@gmail.com>
2021-08-02 17:03:49 +0200

    testing

--- packages/local-client/src/components/cell-list/cell-list-item/cell-list-item.tsx
+++ packages/local-client/src/components/cell-list/cell-list-item/cell-list-item.tsx
@@ -10,6 +10,7 @@ interface CellListItemProps {
 
 export const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
+
   if (cell.type === 'code') {
     child = (
       <>
